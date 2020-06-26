3ee037774c584d3dd16d67acdea0feb315fc0f77
guolu <1448896454@qq.com>
2020-06-26 22:14:58 +0800

    update example deps.ts

--- example/deps.ts
+++ example/deps.ts
@@ -3,6 +3,6 @@ export {
     Route,
     routesBind,
     createRoutes,
-} from "../mod.ts";
-  
+} from "https://deno.land/x/deno_http_router/mod.ts";
+
 export * as server from "https://deno.land/std@0.58.0/http/server.ts";
